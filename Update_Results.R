8b5904ff1146be50a80954605d62e5597268d674
bgautijonsson <bgautijonsson@gmail.com>
2020-08-17 12:48:33 +0000

    Keyrsla dagsins. Virkar að keyra í gegn hjá mér (Binni)

--- Update_Results.R
+++ Update_Results.R
@@ -4,7 +4,7 @@ source("Make_Model_Preds.R")
 source("Make_RW_SIR_Model.R")
 source("Make_Figures.R")
 
-Make_IGLGM_Model(warmup = 100, iter = 100, threads_per_chain = 2)
+Make_IGLGM_Model(warmup = 500, iter = 500, threads_per_chain = 2)
 
 Make_Model_Preds()
 
