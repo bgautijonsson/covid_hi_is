bdf9a78dcc0d91bbb3470d74e61fcc08abdb59fd
karirogg <karirogg@gmail.com>
2020-09-10 17:27:52 +0000

    shiny theme changed, split into two panels

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,32 +1,41 @@
 library(shiny)
 library(gridExtra)
+library(shinythemes)
 
 shinyUI(fluidPage( 
-    includeCSS("styles.css"),
+    theme = shinytheme("flatly"),
     
-    titlePanel("COVID-19 Sviðsmyndir"),
-    sidebarLayout(
-        sidebarPanel(
-            h4('Aðgerð yfirvalda'),
-            checkboxInput("quarantine", "Virk smitrakning", F),
-            
-            radioButtons("test", "Skimun við landamærin:",
-                         c("Engin skimun" = "none",
-                            "Ein skimun" = "first",
-                           "Tvær skimarnir fyrir Íslendinga" = "second_ice",
-                           "Tvær skimanir fyrir alla" = "second_all")),
-            actionButton('go',label="Teikna myndir"),
-            br(),
-            br(),
-            br(),
-            downloadButton('downloadPlot', label = "Hlaða niður mynd sem PDF"),
-            br(),
-            br(),
-            downloadButton('downloadCSV', label='Hlaða niður gögnum sem XLSX')
+    navbarPage("COVID-19 Sviðsmyndir",
+        tabPanel("Sviðsmyndir",
+            sidebarLayout(
+                sidebarPanel(
+                    h4('Aðgerð yfirvalda'),
+                    checkboxInput("quarantine", "Virk smitrakning", F),
+                    
+                    radioButtons("test", "Skimun við landamærin:",
+                                 c("Engin skimun" = "none",
+                                    "Ein skimun" = "first",
+                                   "Tvær skimarnir fyrir Íslendinga" = "second_ice",
+                                   "Tvær skimanir fyrir alla" = "second_all")),
+                    actionButton('go',label="Teikna myndir"),
+                    br(),
+                    br(),
+                    br(),
+                    downloadButton('downloadPlot', label = "Hlaða niður mynd sem PDF"),
+                    br(),
+                    br(),
+                    downloadButton('downloadCSV', label='Hlaða niður gögnum sem XLSX')
+                ),
+                
+                mainPanel(
+                    plotOutput('epidemia_plot',height = '800px')
+                )
+            )
         ),
-        
-        mainPanel(
-            plotOutput('epidemia_plot',height = '800px')
+        tabPanel('Um verkefnið',
+            mainPanel(
+                h4('Um verkefnið')
+            )         
         )
     )
 ))
